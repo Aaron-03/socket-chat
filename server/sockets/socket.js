9e560a9b3cc6c0f63e4d4fb16639ad1969641604
Aarón Huamán <aaron20hc@gmail.com>
2020-05-01 21:28:16 -0500

    Chat Finalizado v1

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -21,6 +21,7 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
 
         callback(usuarios.getPersonasPorSala(data.sala));    
     });
@@ -31,6 +32,8 @@ io.on('connection', (client) => {
 
         const mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
